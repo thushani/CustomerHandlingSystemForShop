66e0dffaf68e08a39625786b345f99310a82313a
unknown <thushaninipu92@gmail.com>
2020-10-12 20:38:56 +0530

    init springboot base project

---
+++ cofeeshop/src/main/java/com/tanda/cofeeshop/CofeeshopApplication.java
@@ -0,0 +1,13 @@
+package com.tanda.cofeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofeeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofeeshopApplication.class, args);
+	}
+
+}
---
+++ cofeeshop/src/test/java/com/tanda/cofeeshop/CofeeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanda.cofeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofeeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
